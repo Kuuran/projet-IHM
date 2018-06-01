532e3ad827dac06208d03df31295747b7133f74f
kuuran <felixjacquemin@gmail.com>
2018-06-01 14:39:29 +0200

    getters and setters + constructeur aeroport

--- src/FlightLive/elements/Airport.java
+++ src/FlightLive/elements/Airport.java
@@ -9,6 +9,15 @@ public class Airport {
     private double latitude;
     private double longitude;
 
+    public Airport(String name, String icao, String country, String city, double latitude, double longitude) {
+        this.name = name;
+        this.icao = icao;
+        this.country = country;
+        this.city = city;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
 
     public double getLongitude() {
         return longitude;
@@ -16,5 +25,45 @@ public class Airport {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcao() {
+        return icao;
+    }
+
+    public void setIcao(String icao) {
+        this.icao = icao;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 }
--- src/FlightLive/elements/Plane.java
+++ src/FlightLive/elements/Plane.java
@@ -7,4 +7,52 @@ public class Plane {
     private Airport depart;
     private Airport arrivee;
     private Airport[] escale;
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Airport getDepart() {
+        return depart;
+    }
+
+    public void setDepart(Airport depart) {
+        this.depart = depart;
+    }
+
+    public Airport getArrivee() {
+        return arrivee;
+    }
+
+    public void setArrivee(Airport arrivee) {
+        this.arrivee = arrivee;
+    }
+
+    public Airport[] getEscale() {
+        return escale;
+    }
+
+    public void setEscale(Airport[] escale) {
+        this.escale = escale;
+    }
 }
