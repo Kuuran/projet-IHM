f80775688579a2952ba32f40f4ce8872a0cbf755
nynerf <ryfabien@gmail.com>
2018-06-01 14:20:50 +0200

    salut

--- src/FlightLive/elements/Airport.java
+++ src/FlightLive/elements/Airport.java
@@ -3,4 +3,18 @@ package FlightLive.elements;
 public class Airport {
 
     private String name;
+    private String icao;
+    private String country;
+    private String city;
+    private double latitude;
+    private double longitude;
+
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
--- src/FlightLive/elements/Plane.java
+++ src/FlightLive/elements/Plane.java
@@ -1,4 +1,10 @@
 package FlightLive.elements;
 
 public class Plane {
+    private double altitude;
+    private double latitude;
+    private double longitude;
+    private Airport depart;
+    private Airport arrivee;
+    private Airport[] escale;
 }
